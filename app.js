df5bd78ff8f0f6ba05e7cad32dc32d534aefccd0
Eric Ju <ericju12@gmail.com>
2019-06-17 09:07:00 -0400

    monday morning

--- app.js
+++ app.js
@@ -10,11 +10,64 @@ var path = d3.geoPath()
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height); //creates the svg
+    .attr("height", height)
+    .attr("x", 0)
+    .attr("y", 0); //creates the svg
 
 // var color = d3.scaleThreshold()
 //     .domain([0.02, 0.04, 0.06, 0.08, 0.10])
 //     .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);
+
+const countyarray = ['36061',
+'02020',
+'06075',
+'15003',
+'36047',
+'25025',
+'11001',
+'53033',
+'42101',
+'06085',
+'17031',
+'06037',
+'37119',
+'06067',
+'41051',
+'12025',
+'36001',
+'27053',
+'24005',
+'08031',
+'42003',
+'37063',
+'06073',
+'37021',
+'36029',
+'13121',
+'04019',
+'55079',
+'48015',
+'12095',
+'12057',
+'39049',
+'32003',
+'28059',
+'47037',
+'32031',
+'29189',
+'04013',
+'48113',
+'18097',
+'48029',
+'48225',
+'21111',
+'55025',
+'47157',
+'49035',
+'05119',]
+
+
+
 
 const cityarray = []
 d3.json("master.json", function(data) {
@@ -23,8 +76,12 @@ d3.json("master.json", function(data) {
         cityarray.push(array[i].name.split(", ")[0])
     }
 });
-
 //this hacky solution does not work - need to map by id... not name
+
+const fulldataset = (d3.json("master.json", function(data) {
+    return data
+    //any other functions that depend on data
+}));
 
 d3.queue()
     .defer(d3.json, "https://d3js.org/us-10m.v2.json")
@@ -52,18 +109,28 @@ function ready (error, us) {
     .append("path")
     .attr("d", path)
     .style("fill", function(d) {
-        if (cityarray.includes(d.properties.name)) {
+        if (countyarray.includes(d.id)) {
             return "yellow"
         }
     })
     .on("mouseover",function(d) {
-        if (cityarray.includes(d.properties.name)) {
-        d3.select("h2").text(d.properties.name)};
+        if (countyarray.includes(d.id)) {
+        d3.select("h2").text(`County: ${d.properties.name}`)
+        d3.select("h3").text()
+        };
     })
     .on("mouseout"), function(d) {
-        if (cityarray.includes(d.properties.name)) {
+        if (countyarray.includes(d.id)) {
             d3.select("h2").text("")};
     };    
+
+   /* svg.apppend("g").attr("class", "items")
+        .selectAll("path")
+        .data(fulldataset)
+        .enter()
+        .append("path")
+        .attr("d")*/
+
         // .style("stroke", "white")
         // .style("fill", "grey");//get ratevalue for property matching dataID
             //pass rate value to color function, return color based on domain and range
